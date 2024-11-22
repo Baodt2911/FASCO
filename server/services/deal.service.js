ae81165c4c10dae7dba23cb2508feb185d445b6d
Baodt2911 <dobao2911bs@gmail.com>
2024-11-22 20:50:59 +0700

    update deal

--- server/services/deal.service.js
+++ server/services/deal.service.js
@@ -1,17 +1,74 @@
-const createDealService = async (req, res) => {
+import _deal from "../models/deal.model.js";
+
+const createDealService = async ({
+  title,
+  description,
+  discount_type,
+  discount_value,
+  start_date,
+  end_date,
+  applied_products,
+  min_order_value,
+  max_discount,
+  usage_limit,
+}) => {
   try {
+    await _deal.create({
+      title,
+      description,
+      discount_type,
+      discount_value,
+      start_date,
+      end_date,
+      applied_products,
+      min_order_value,
+      max_discount,
+      usage_limit,
+    });
+    return {
+      status: 201,
+      message: "Created successfully!",
+    };
   } catch (error) {
     console.log(error);
   }
 };
-const getDealService = async (req, res) => {
+const getDealService = async () => {
   try {
+    const deals = await _deal.find({ status: "active" }).populate({
+      path: "applied_products",
+      model: "products",
+      select: ["name", "photos"],
+      populate: {
+        path: "photos",
+        model: "photos",
+        select: ["url"],
+      },
+    });
+
+    return {
+      status: 200,
+      element: deals,
+    };
   } catch (error) {
     console.log(error);
   }
 };
-const updateDealService = async (req, res) => {
+const updateDealService = async ({ _id }) => {
   try {
+    const isDeal = await _deal.findByIdAndUpdate(_id, {
+      status: "inactive",
+    });
+    if (!isDeal) {
+      return {
+        status: 404,
+        message: "Deal is not found",
+      };
+    }
+    return {
+      status: 200,
+      message: "Updated successfully!",
+    };
   } catch (error) {
     console.log(error);
   }
